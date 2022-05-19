269c4a4df3e715f2104036d0fa47b02ea2fc14c6
Ariana Marchi <arianalaplata@hotmail.com>
2022-05-19 03:01:59 -0300

    agrego cambios en la interfaz de inicio de sesión

--- main/urls.py
+++ main/urls.py
@@ -4,10 +4,6 @@ app_name= 'main'
 
 urlpatterns = [
     path('', views.homepage,name= 'homepage'),
-<<<<<<< HEAD
-    path('registro/', views.registro , name="registro")
-
-=======
+    path('registro/', views.registro , name="registro"),
     path('', views.login, name= 'login' )
->>>>>>> 8a1dbd2616431fc6a980c63c0c611e31898e16e5
 ]
