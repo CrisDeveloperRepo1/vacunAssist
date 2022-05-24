c432afe435e37bc72ae579a87609cc9eb11a435b
mmaurino <martmauri29@hotmail.com>
2022-05-23 23:58:39 -0300

    opcion de registro

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ app_name= 'main'
 
 urlpatterns = [
     path('', views.homepage,name= 'homepage'),
-    path('registro/', views.registro , name="registro"),
+    path('login/registro/', views.registro , name="registro"),
     path('login/', views.login, name= 'login' ),
     path('login/verificación/' , views.verificar, name= "validar"),
     path('inicio_admin/' , views.inicio_adm, name= "inicio_adm"),
