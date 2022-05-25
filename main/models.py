ffe1ea66d030db65fd32432f044871d560c10b49
cristian <cristiantorrez_@hotmail.com>
2022-05-25 16:42:10 -0300

    cambios en bs

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,18 +7,12 @@ class Vacunador(models.Model):
 
     vacunador_nombre= models.CharField(max_length=200)
 
-
-    # vacunador_apellido= models.CharField(max_length=20)
-    # vacunador_fechaNac= models.DateTimeField("fecha nacimiento")
-    # vacunador_zona= models.CharField(max_length=200)
-    # vacunador_dni= models.IntegerField()
-    # vacunador_email= models.EmailField(max_length=254)
-
     vacunador_apellido= models.CharField(max_length=20)
     vacunador_fechaNac= models.DateTimeField("fecha nacimiento")
     vacunador_zona= models.CharField(max_length=200)
     vacunador_dni= models.IntegerField()
     vacunador_email= models.EmailField(max_length=254)
+    vacunador_codigo= models.IntegerField()
 
 
     def __str__(self):
@@ -27,13 +22,9 @@ class Paciente(models.Model):
     opciones=((1, 'Si'), (2, 'No'))
     paciente_nombre= models.CharField(max_length=200)
     paciente_apellido= models.CharField(max_length=20)
-
-
-
-
     paciente_fechaNac= models.DateTimeField("fecha nacimiento")
     paciente_zona= models.CharField(max_length=200)
-    paciente_dni= models.IntegerField()
+    paciente_dni= models.CharField(max_length=200)
     paciente_email= models.EmailField(max_length=254)
     vac_Gripe_aplicada=models.SmallIntegerField(choices=opciones,default=2)
     vac_Gripe_turno= models.DateTimeField("turno_gripe")
@@ -47,6 +38,7 @@ class Paciente(models.Model):
     vac_Covid_turno2= models.DateTimeField("turno_2da")
     vac_Covid1era_asistencia=models.SmallIntegerField(choices=opciones,default=2)
     vac_Covid2da_asistencia=models.SmallIntegerField(choices=opciones,default=2)
+    paciente_codigo= models.IntegerField()
 
     def __str__(self):
         return self.paciente_nombre
@@ -59,6 +51,7 @@ class Administrador(models.Model):
     administrador_dni= models.IntegerField()
     administrador_contraseña = models.IntegerField()
     administrador_email= models.EmailField(max_length=254)
+    administrador_codigo= models.IntegerField()
 
     def __str__(self):
         return self.administrador_nombre
